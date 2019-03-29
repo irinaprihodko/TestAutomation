3f8f67fd6f55b52b163a22c6a712eb01456ce854
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-03-29 22:56:29 +0200

    Create MobilePhones.java

---
+++ Lesson1/MobilePhones.java
@@ -0,0 +1,15 @@
+package Lesson1;
+
+public class MobilePhones {
+    int weight;
+    String color;
+    String model;
+    int price;
+    boolean isOutOfServise;
+
+    void makeACall() {
+    }
+    void charging() {
+    }
+    void turnOff() {
+    }}
