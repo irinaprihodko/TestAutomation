2208ba4de76148f54b41e4b99adf4b137adda80e
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-03-29 22:56:07 +0200

    Create FluorescentTubes.java

---
+++ Lesson1/FluorescentTubes.java
@@ -0,0 +1,15 @@
+package Lesson1;
+
+public class FluorescentTubes {
+    int length;
+    String colorGlowing;
+    int power;
+    int price;
+    boolean isWhole;
+
+    void startGlowing() {
+    }
+    void changeColor() {
+    }
+    void turnOff() {
+    }}
