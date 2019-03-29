c6762f2308bb6a3ade24c2e16f49758c46e8989d
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-03-29 22:55:46 +0200

    Create AlcoholisBeverages.java

---
+++ Lesson1/AlcoholisBeverages.java
@@ -0,0 +1,15 @@
+package Lesson1;
+
+public class AlcoholicBeverages {
+    int price;
+    String color;
+    float strengh;
+    String container;
+    boolean isSold;
+
+    void freeFromContainer() {
+    }
+    void degustate() {
+    }
+    void confiscate() {
+    }}
