0b5b270cec975570212a2098ec16539c912e3a44
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-03-29 22:56:35 +0200

    Create PCs.java

---
+++ Lesson1/PCs.java
@@ -0,0 +1,15 @@
+package Lesson1;
+
+public class PCs {
+    int storage;
+    String company;
+    int power;
+    int price;
+    boolean isWorking;
+
+    void runSkype() {
+    }
+    void updatingWindows() {
+    }
+    void explode() {
+    }}
