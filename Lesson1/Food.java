45cee41904bf905b7b5721903b6ff2c631e842e3
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-03-29 22:56:14 +0200

    Create Food.java

---
+++ Lesson1/Food.java
@@ -0,0 +1,15 @@
+package Lesson1;
+
+public class Food {
+    int termin;
+    String color;
+    int weight;
+    boolean isInPackage;
+    boolean isNeedToFreeze;
+
+    void GetRidIfSpoiled() {
+    }
+    void Eat() {
+    }
+    void putInFridge() {
+    }}
