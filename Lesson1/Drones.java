04386ea96bf28953b34f77c57ac1405d1935e01e
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-03-29 22:56:00 +0200

    Create Drones.java

---
+++ Lesson1/Drones.java
@@ -0,0 +1,16 @@
+package Lesson1;
+
+public class Drones{
+    int width;
+    String color;
+    int weight;
+    int price;
+    int storage;
+    boolean isWorking;
+
+    void startRecordVideo() {
+    }
+    void finishRecordVideo() {
+    }
+    void launch() {
+    }}
