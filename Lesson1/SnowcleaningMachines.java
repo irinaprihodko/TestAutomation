b4a2ebd4bb518fb35510d045ee1b334dad411eab
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-03-29 22:56:41 +0200

    Create SnowcleaningMachines.java

---
+++ Lesson1/SnowcleaningMachines.java
@@ -0,0 +1,15 @@
+package Lesson1;
+
+public class SnowcleaningMachines {
+    int power;
+    String model;
+    float weight;
+    String usability;
+    boolean isEasyToUse;
+
+    void launch() {
+    }
+    void stuck() {
+    }
+    void startSelling() {
+    }}}
