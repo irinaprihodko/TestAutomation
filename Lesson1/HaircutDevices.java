7b5ee69ac985957977e71b030fddae10117ae2a9
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-03-29 22:56:20 +0200

    Create HaircutDevices.java

---
+++ Lesson1/HaircutDevices.java
@@ -0,0 +1,15 @@
+package Lesson1;
+
+public class HaircutDevices {
+    float sharpness;
+    String company;
+    int power;
+    int price;
+    boolean isUkrainian;
+
+    void changeSpeed() {
+    }
+    void turnOn() {
+    }
+    void charge() {
+    }}
