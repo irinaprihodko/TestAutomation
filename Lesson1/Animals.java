e1dca80008f84fb47bea05d296d3e1ba263ca77a
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-03-29 22:55:54 +0200

    Create Animals.java

---
+++ Lesson1/Animals.java
@@ -0,0 +1,72 @@
+package Lesson1;
+
+public class Animals{
+
+    int age; //возраст животного, тип данных – целые числа int
+
+    int weight; //вес животного, тип данных – целые числа int
+
+    int height; //высота животного, тип данных – целые числа int
+
+    String color; //цвет животного, тип данных – текстовая строка String
+
+    boolean isWool; //атрибут наличия шерсти, тип данных – булева переменная, принимающая
+    значения true или false
+
+    void goTosleep(){
+
+    }
+    void startEating(){
+    }
+    void startRunning(){
+    }
+}
+public class Food {
+    int termin;
+    String color;
+    int weight;
+    boolean isInPackage;
+    boolean isNeedToFreeze;
+
+    void GetRidIfSpoiled() {
+    }
+    void Eat() {
+    }
+    void putInFridge() {
+    }
+
+    public class Drones {
+        int width;
+        String color;
+        int weight;
+        int price;
+        int storage;
+        boolean isWorking;
+
+        void startRecord() {
+        }
+        void finishRecord() {
+        }
+        void launch() {
+        }
+        public class Wardrobes {
+            int width;
+            String colorCase;
+            int length;
+            int doorsAmount;
+            boolean isDelivered;
+
+            void startRecord() {
+            }
+            void finishRecord() {
+            }
+            void launch() {
+            }
+}
+
+
+
+
+
+
+
