5d0913658d4843db1791c4afc7bbd0de5f698268
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-03-29 22:56:47 +0200

    Create Wardrobes.java

---
+++ Lesson1/Wardrobes.java
@@ -0,0 +1,16 @@
+package Lesson1;
+
+public class Wardrobes {
+    float width;
+    String colorCase;
+    float length;
+    int doorsAmount;
+    boolean isDelivered;
+
+    void open() {
+    }
+    void close() {
+    }
+    void takeClothes() {
+    }}
+
