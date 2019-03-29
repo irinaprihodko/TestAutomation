78c24df87cd6fcf808046d09839d5e790a6e6106
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-03-29 22:55:37 +0200

    Create Airplanes.java

---
+++ Lesson1/Airplanes.java
@@ -0,0 +1,15 @@
+package Lesson1;
+
+public class Airplanes {
+    int length;
+    String company;
+    int weight;
+    int capacity;
+    boolean isForPassengers;
+
+    void landing() {
+    }
+    void taking off() {
+    }
+    void hidingLandingGear() {
+    }}
