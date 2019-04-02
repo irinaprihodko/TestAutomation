68ae66ab05c2ab6fb63c051b823ab0a014eba085
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-04-02 15:46:15 +0300

    Create Main.java

---
+++ Lesson2extended/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Alcohol a = new Alcohol("wine",20);
+        Alcoholic alcoholic = new Alcoholic ("Jack", "Daniels", a );
+        Pilot pilot  = new Pilot("Peter"," Pan", 30, 75);
+
+
+
+
+    }
+}
