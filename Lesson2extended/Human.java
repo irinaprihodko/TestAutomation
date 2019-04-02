75c5d825c5fabd98fe4aaa5ff15ceab03d05d9a9
irinaprihodko <irinaprihodkoirina@gmail.com>
2019-04-02 15:46:09 +0300

    Update Human.java

--- Lesson2extended/Human.java
+++ Lesson2extended/Human.java
@@ -1,10 +1,24 @@
 public class Human {
 
-        protected String name;
-        protected String surname;
-        public void setName(String newName) {name = newName}
-        public String getName(){ return name;}
-        public void setSurname(String newSurname){surname = newSurname}
-        public String getSurname(){ return surname;}}
+    protected String name;
+    protected String surname;
 
-    
+    public Human(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Human (){
+        this. name = "Unknown";
+        this.surname = "";
+    }
+
+
+    public void setName(String newName) {name = newName;}
+    public String getName(){ return name;}
+    public void setSurname(String newSurname){surname = newSurname;}
+    public String getSurname(){ return surname;}
+
+}
+
+
